114b1aa967a5c4942fdd1f0051ebd467e40d637e
kstiopin <kirill.stiopin@hotmail.com>
2020-07-20 19:39:42 +0300

    update Spares to show last repair mileage and how long till the next one

--- src/Spares.js
+++ src/Spares.js
@@ -3,7 +3,20 @@ import { useSelector } from 'react-redux';
 import { selectedCar$ } from './selectors';
 
 const Spares = () => {
-    const { spares } = useSelector(selectedCar$);
+    const { mileage, repairs, spares } = useSelector(selectedCar$);
+
+    const renderSpareRow = spare => {
+        const lastRepair = repairs.find(({ spare_id }) => spare_id === spare.spareId);
+
+        return (
+            <tr key={ spare.spareId }>
+                <td>{ spare.spareName }</td>
+                <td>{ spare.mileage }</td>
+                <td>{ lastRepair && lastRepair.mileage }</td>
+                <td>{ (lastRepair ? lastRepair.mileage * 1 : 0) + spare.mileage * 1 - mileage * 1 }</td>
+                <td>{ spare.additional }</td>
+            </tr>);
+    };
 
     return (
         <div className='spares'>
@@ -12,15 +25,13 @@ const Spares = () => {
                     <tr>
                         <th>Название</th>
                         <th>Период</th>
+                        <th>Последняя замена</th>
+                        <th>До замены</th>
                         <th>Описание</th>
                     </tr>
                 </thead>
                 <tbody>
-                    { spares.map(({ additional, mileage, spareId, spareName }) => <tr key={ spareId }>
-                        <td>{ spareName }</td>
-                        <td>{ mileage }</td>
-                        <td>{ additional }</td>
-                    </tr>) }
+                    { spares.map(renderSpareRow) }
                 </tbody>
             </table>
         </div>);
